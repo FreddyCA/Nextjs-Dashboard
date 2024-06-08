c7362d0377be054b2b77bcc72d98fca5799f4cb4
Freddy Coaricona <fcoaricona@outlook.com>
2024-06-07 21:13:51 -0400

    Refactor Button Logo Login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,9 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
- 
 
 import { Metadata } from 'next';
- 
+import Link from 'next/link';
+
 export const metadata: Metadata = {
   title: 'Login',
 };
@@ -15,8 +15,19 @@ export default function LoginPage() {
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="w-32 text-white md:w-36">
-            <AcmeLogo />
+            <Link href={'/'}>
+              <AcmeLogo />
+            </Link>
+            {/* <AcmeLogo /> */}
           </div>
+          {/* <Link
+            className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+            href="/"
+          >
+            <div className="w-32 text-white md:w-40">
+              <AcmeLogo />
+            </div>
+          </Link> */}
         </div>
         <LoginForm />
       </div>
